31a142a74027a6360c0093c6bc9c0451fa92d7a5
Victor <victorborisov1990@gmail.com>
2023-06-09 22:55:03 +0300

    первый комит.базовый функционал

---
+++ data_create.py
@@ -0,0 +1,16 @@
+def set_title(): #ex name_data
+    title = input('Введите название заметки: ')
+    return title
+
+def set_data(): #ex surname_data
+    data = input('Введите текст заметки: ')
+    return data
+
+# def second_name_data():
+#     second_name = input('Введите отчество: ')
+#     return second_name
+
+# def phone_data():
+#     phone = input('Введите телефон: ')
+#     return phone
+
